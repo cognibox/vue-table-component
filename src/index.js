46e33884e4a7371d33c9e827a627d09b984c014f
Julien de Carufel <jcarufel@cognibox.com>
2019-10-28 13:04:14 -0400

    lint code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import TableColumn from './components/TableColumn';
 import TableComponent from './components/TableComponent';
 import Pagination from './components/Pagination';
-import { mergeSettings } from './settings';
 
 import Vue from 'vue';
 import TableExample from './components/TableExample';
