50bd56895e81900cde33c817a9a32bbb7a8664bc
Julien de Carufel <jcarufel@cognibox.com>
2019-10-28 13:01:55 -0400

    remove install function

--- src/index.js
+++ src/index.js
@@ -12,18 +12,4 @@ if (process.env.NODE_ENV !== 'production') {
   }).$mount('#app');
 }
 
-export default {
-  install(vue, options = {}) {
-    mergeSettings(options);
-
-    vue.component('table-component', TableComponent);
-    vue.component('table-column', TableColumn);
-    vue.component('pagination', Pagination);
-  },
-
-  settings(settings) {
-    mergeSettings(settings);
-  },
-};
-
 export { TableComponent, TableColumn, Pagination };
