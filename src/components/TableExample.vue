cf23729238b9b53c3331374bc0612da9a13cbf5f
Julien de Carufel <jcarufel@cognibox.com>
2019-11-20 15:45:00 -0500

    CBX-7501 create table column mixin

--- src/components/TableExample.vue
+++ src/components/TableExample.vue
@@ -1,26 +1,26 @@
 <template>
   <TableComponent
-    :data="[{ a: 1 }, { a: 2 }]"
+    :data="[{ a: 1, b: '1' }, { a: 2, b: 2 }]"
     :pagination="{ perPage: 1, currentPage: 1 }"
   >
-    <TableColumn show="a" />
+    <TableColumn
+      show="a"
+      :sortable="true"
+    />
+    <TableColumnExample />
   </TableComponent>
 </template>
 
 <script>
 import TableComponent from './TableComponent.vue';
 import TableColumn from './TableColumn.vue';
+import TableColumnExample from './TableColumnExample.vue';
 
 export default {
   components: {
     TableComponent,
     TableColumn,
+    TableColumnExample,
   },
-
-  data: () => ({
-    columns: [],
-    rows: [],
-    localSettings: {},
-  }),
 };
 </script>
